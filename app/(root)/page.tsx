4ec3dc3c6c56bb189b822db98d046b9398fe029e
Marina Zonova <114925418+marinazonz@users.noreply.github.com>
2023-07-03 12:11:29 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+            <p>Lets get started!</p>
+            <Button size='default' variant='default'>
+                Click here
+            </Button>
+        </div>
+    );
+}
